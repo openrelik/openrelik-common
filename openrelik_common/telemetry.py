3efee8df323127be1cb27e8b783635e9604b221a
hacktobeer <ramsesdebeer@gmail.com>
2025-07-30 11:17:54 +0200

    Fix small opentelemetry library bugs. (#7)
    * fix small bugs in telemetry.py
    * add relevant poetry modules

--- openrelik_common/telemetry.py
+++ openrelik_common/telemetry.py
@@ -37,6 +37,7 @@ import json
 import os
 
 from opentelemetry import trace
+from opentelemetry.trace.span import INVALID_SPAN
 
 from opentelemetry.exporter.otlp.proto.grpc import trace_exporter as grpc_exporter
 from opentelemetry.exporter.otlp.proto.http import trace_exporter as http_exporter
@@ -46,6 +47,7 @@ from opentelemetry.sdk.resources import Resource
 from opentelemetry.sdk.trace import TracerProvider
 from opentelemetry.sdk.trace.export import BatchSpanProcessor
 
+
 def setup_telemetry(service_name: str):
     """Configures the OpenTelemetry trace exporter.
 
@@ -53,28 +55,28 @@ def setup_telemetry(service_name: str):
         service_name (str): the service name used to identify generated traces.
     """
 
-    resource = Resource(attributes={
-        "service.name": service_name
-    })
-
-    # --- Tracing Setup ---
-    trace.set_tracer_provider(TracerProvider(resource=resource))
+    resource = Resource(attributes={"service.name": service_name})
 
     otel_mode = os.environ.get("OPENRELIK_OTEL_MODE", "otlp-grpc")
     otlp_grpc_endpoint = os.environ.get("OPENRELIK_OTLP_GRPC_ENDPOINT", "jaeger:4317")
-    otlp_http_endpoint = os.environ.get("OPENRELIK_OTLP_HTTP_ENDPOINT", "http://jaeger:4318/v1/traces")
+    otlp_http_endpoint = os.environ.get(
+        "OPENRELIK_OTLP_HTTP_ENDPOINT", "http://jaeger:4318/v1/traces"
+    )
 
     trace_exporter = None
     if otel_mode == "otlp-grpc":
         trace_exporter = grpc_exporter.OTLPSpanExporter(
-                endpoint=otlp_grpc_endpoint, insecure=True)
+            endpoint=otlp_grpc_endpoint, insecure=True
+        )
     elif otel_mode == "otlp-http":
-        trace_exporter = http_exporter.OTLPSpanExporter(
-                endpoint=otlp_http_endpoint)
+        trace_exporter = http_exporter.OTLPSpanExporter(endpoint=otlp_http_endpoint)
     else:
         raise Exception("Unsupported OTEL tracing mode %s", otel_mode)
 
-    trace.get_tracer_provider().add_span_processor(BatchSpanProcessor(trace_exporter))
+    # --- Tracing Setup ---
+    trace_provider = TracerProvider(resource=resource)
+    trace_provider.add_span_processor(BatchSpanProcessor(trace_exporter))
+    trace.set_tracer_provider(trace_provider)
 
 
 def instrument_celery_app(celery_app):
@@ -92,7 +94,7 @@ def instrument_fast_api(fast_api):
     Args:
         fast_api (fastapi.FastAPI): the FastAPI app to instrument.
     """
-    FastAPIInstrumentor.instrument_app(api_v1)
+    FastAPIInstrumentor.instrument_app(fast_api)
 
 
 def add_attribute_to_current_span(name: str, value: object):
@@ -104,5 +106,5 @@ def add_attribute_to_current_span(name: str, value: object):
         value (object): the value of the attribute. This needs to be a json serializable object.
     """
     otel_span = trace.get_current_span()
-    if otel_span != trace.span.INVALID_SPAN:
+    if otel_span != INVALID_SPAN:
         otel_span.set_attribute(name, json.dumps(value))
